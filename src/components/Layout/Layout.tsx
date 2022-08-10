bb5d77747eab9ccd52c26f2464f8d002b8c466cb
fp <f.piacitelli@gmail.com>
2022-08-10 12:28:19 +0200

    first commit

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,46 @@
+import { Grid } from '@mui/material';
+import { Box } from '@mui/system';
+import { Footer, Header } from '@pagopa/selfcare-common-frontend';
+import { useSelector } from 'react-redux';
+import React from 'react';
+import { useUnloadEventOnExit } from '@pagopa/selfcare-common-frontend/hooks/useUnloadEventInterceptor';
+import { userSelectors } from '@pagopa/selfcare-common-frontend/redux/slices/userSlice';
+import { ENV } from '../../utils/env';
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  const onExit = useUnloadEventOnExit();
+  const loggedUser = useSelector(userSelectors.selectLoggedUser);
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
+      <Header
+        withSecondHeader={false}
+        onExit={onExit}
+        assistanceEmail={ENV.ASSISTANCE.ENABLE ? ENV.ASSISTANCE.EMAIL : undefined}
+        enableLogin={true}
+        loggedUser={
+          loggedUser
+            ? {
+                id: loggedUser ? loggedUser.uid : '',
+                name: loggedUser?.name,
+                surname: loggedUser?.surname,
+                email: loggedUser?.email,
+              }
+            : false
+        }
+      />
+      <Grid container>{children}</Grid>
+      <Footer onExit={onExit} loggedUser={!!loggedUser} />
+    </Box>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,41 @@
+import '@pagopa/selfcare-common-frontend/common-polyfill';
+import '@pagopa/selfcare-common-frontend/index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from '@pagopa/mui-italia/theme';
+import { CONFIG } from '@pagopa/selfcare-common-frontend/config/env';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { store } from './redux/store';
+import { MOCK_USER } from './utils/constants';
+import { ENV } from './utils/env';
+import './locale';
+
+// eslint-disable-next-line functional/immutable-data
+CONFIG.MOCKS.MOCK_USER = MOCK_USER;
+// eslint-disable-next-line functional/immutable-data
+CONFIG.URL_FE.LOGIN = `${ENV.URL_FE.LOGIN}?onSuccess=assistenza`;
+// eslint-disable-next-line functional/immutable-data
+CONFIG.URL_FE.LOGOUT = ENV.URL_FE.LOGOUT;
+// eslint-disable-next-line functional/immutable-data
+CONFIG.URL_FE.ASSISTANCE = '/assistance';
+import './consentAndAnalyticsConfiguration.ts';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
