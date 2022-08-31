476257b5b49cb6b344e74f10b83dfec551b78884
Elena Signore <92723520+esignore@users.noreply.github.com>
2022-08-31 17:45:59 +0200

    Release dev (#10)
    * Fix success login url (#3)
    
    * add simple test to check code review pipeline
    * fix success login url
    
    * Selc 1435 modify token exchange layout 
    
    * add simple test to check code review pipeline
    
    * Fix success login url (#3)
    
    * add simple test to check code review pipeline
    * fix success login url
    
    * SELC-1435 - modified TokenExchang layout and updated mui to latest version
    
    * SELC-1435 - fixed test
    
    * SELC-1435 - merge done
    
    * SELC-1435 - added button in error page
    
    * SELC-1435 - test temporarily modified
    
    * SELC-1435 - restored tests
    
    * SELC-1435 - added variable to adding space
    
    * SELC-1435 - decreased memory
    
    * SELC-1435 - increased build memory
    
    Co-authored-by: fp <f.piacitelli@gmail.com>
    
    * Selc 1435 modify token exchange layout
    
    * add simple test to check code review pipeline
    
    * Fix success login url (#3)
    
    * add simple test to check code review pipeline
    * fix success login url
    
    * SELC-1435 - modified TokenExchang layout and updated mui to latest version
    
    * SELC-1435 - fixed test
    
    * SELC-1435 - merge done
    
    * SELC-1435 - added button in error page
    
    * SELC-1435 - test temporarily modified
    
    * SELC-1435 - restored tests
    
    * SELC-1435 - added variable to adding space
    
    * SELC-1435 - decreased memory
    
    * SELC-1345 - increased build memory
    
    * SELC-143 - increased space in the pipeline
    
    Co-authored-by: fp <f.piacitelli@gmail.com>
    
    * Selc 1435 merge
    
    * Fix success login url (#3)
    
    * add simple test to check code review pipeline
    * fix success login url
    
    * SELC-1435
    
    Co-authored-by: Federico Piacitelli <f.piacitelli@gmail.com>
    
    * Selc 1435 fix (#9)
    
    * Fix success login url (#3)
    
    * add simple test to check code review pipeline
    * fix success login url
    
    * fixed test
    
    Co-authored-by: Federico Piacitelli <f.piacitelli@gmail.com>
    
    * Selc 1435
    
    * Fix success login url (#3)
    
    * add simple test to check code review pipeline
    * fix success login url
    
    * SELC-1435
    
    Co-authored-by: Federico Piacitelli <f.piacitelli@gmail.com>
    
    * Fix success login url (#3) (#12)
    
    * add simple test to check code review pipeline
    * fix success login url
    
    Co-authored-by: Federico Piacitelli <f.piacitelli@gmail.com>
    
    Co-authored-by: Federico Piacitelli <f.piacitelli@gmail.com>
    Co-authored-by: andrea-putzu <106688558+andrea-putzu@users.noreply.github.com>
    Co-authored-by: Andrea Putzu <andrea.putzu@pagopa.it>

--- src/locale/it.ts
+++ src/locale/it.ts
@@ -1,6 +1,11 @@
 export default {
-  tokenExchangePage:{
+  tokenExchangePage: {
     title: 'Token Exchange',
     subTitle: 'Verrai presto reindirizzato alla pagina desiderata',
-  }
+    error: {
+      title: 'Qualcosa è andato storto',
+      subTitle: `A causa di un errore del sistema non è possibile completare la procedura. <1/> Ti chiediamo di riprovare più tardi.`,
+      // errorButtoknLabel: 'Riprova',
+    },
+  },
 };
