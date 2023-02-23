eb84b9cfa7771e20fa58d3bd74afcf7da1505f83
esignore <elena.signore@nttdata.com>
2023-02-23 16:32:25 +0100

    SELC-2033 - temp commit

--- src/pages/TokenExchange/TokenExchange.tsx
+++ src/pages/TokenExchange/TokenExchange.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Box, Button, CircularProgress, Grid, Typography } from '@mui/material';
-import { storageTokenOps } from '@pagopa/selfcare-common-frontend/utils/storage';
+// import { storageTokenOps } from '@pagopa/selfcare-common-frontend/utils/storage';
 import { Trans } from 'react-i18next';
 import withLogin from '@pagopa/selfcare-common-frontend/decorators/withLogin';
 import { IllusError } from '@pagopa/mui-italia';
@@ -19,22 +19,33 @@ const TokenExchange = () => {
   // const { t } = useTranslation();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const token = storageTokenOps.read();
+  // const token = storageTokenOps.read();
+  const token =
+    'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Imp3dF8yNDpiMjo0ODpmZTo3MTpjNTplMzoxOTo1Nzo1ODowYzphNzoxNTo5NDplZDphMyJ9.eyJlbWFpbCI6ImRtYXJ0aW5vQGxpdmUuY29tIiwiZmFtaWx5X25hbWUiOiJMb25nbyIsImZpc2NhbF9udW1iZXIiOiJMTkdNTEU4NVAxOUM4MjZKIiwibmFtZSI6IkVtaWxpYSIsImZyb21fYWEiOmZhbHNlLCJ1aWQiOiJiOWI4OWVmOS00ZGNiLTRlMjctODE5Mi1kOTcyZWZlZjYxNGUiLCJsZXZlbCI6IkwyIiwiaWF0IjoxNjc3MTYxNDkyLCJleHAiOjE2NzcxOTM4OTIsImF1ZCI6ImFwaS51YXQuc2VsZmNhcmUucGFnb3BhLml0IiwiaXNzIjoiU1BJRCIsImp0aSI6IjAxR1NaOVRXQTE2RzM2ODBGNjdIM0FQNk00In0.tTvKzzQTt721T3BRX65-PjRxKZSRWDZ1TDxwcabwWvw20TkDbBBu1iY1blIVnkC8hnbfQKPESmDd8hjVC5FfYCA6s5urBHbCjHBc1q363gP4SshmrG6qRCsoZktuBwkVe-qs4QzuOvscnmGlzAXepGfMAGCxbPDE2MtZpNDejLXV33duFh4no62V3OuRewrQHpb9dU19pAWjnat4WV1lFSSBe1R5KPs71LLJ2n_njqU2feuJAY31jOvXwHyg8Hcw6H7fHyeMSiFZuKjV-KY2xA1qgKIMjzsV0mJjTIDNCRnRkhSzR6mqkdI62fSNANSZdFeN5FEWnt_7t2rjYozMvw';
 
   useEffect(() => {
     const productId = new URLSearchParams(window.location.search).get('productId');
     const institutionId = new URLSearchParams(window.location.search).get('institutionId');
-
+    const uid = new URLSearchParams(window.location.search).get('uid');
+    console.log('uid', uid);
     if (productId && institutionId) {
-      retrieveProductBackofficeURL(productId, institutionId);
+      if (uid) {
+        retrieveProductBackofficeURL(productId as string, institutionId as string, uid);
+      } else {
+        retrieveProductBackofficeURL(productId, institutionId);
+      }
     }
   }, []);
 
-  const retrieveProductBackofficeURL = (productId: string, institutionId: string) => {
+  const retrieveProductBackofficeURL = (productId: string, institutionId: string, uid?: string) => {
     setLoading(true);
-
+    console.log('xx uid fetch', uid);
     fetch(
-      `${ENV.URL_API.API_DASHBOARD}/products/${productId}/back-office?institutionId=${institutionId}`,
+      `${
+        ENV.URL_API.API_DASHBOARD
+      }/products/${productId}/back-office?institutionId=${institutionId}${
+        uid ? `?uid=${uid}` : ''
+      }`,
       {
         method: 'GET',
         credentials: 'include',
@@ -51,9 +62,9 @@ const TokenExchange = () => {
         throw new Error('Something went wrong');
       })
       .then((data) => {
-        console.log(data);
-        // eslint-disable-next-line functional/immutable-data
-        window.location.href = data;
+        console.log('data: ', data);
+        // eslint-disable-next-line functional/immutable-data, sonarjs/no-nested-template-literals
+        window.location.href = `${data}${uid ? `?uid=${uid}` : ''}`;
       })
       .catch((error) => {
         setError(true);
