3abaf703bf940c4dfcfa183b922861f979cdb246
Federico Piacitelli <f.piacitelli@gmail.com>
2023-10-27 11:56:51 +0200

    PASELC-583 add environment management (#62) (#63)

--- src/pages/TokenExchange/__tests__/TokenExchange.test.tsx
+++ src/pages/TokenExchange/__tests__/TokenExchange.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import TokenExchange from '../TokenExchange';
 import '../../../locale';
 import { createStore } from './../../../redux/store';
@@ -6,6 +6,22 @@ import { verifyMockExecution as verifyLoginMockExecution } from '../../../__mock
 import { Provider } from 'react-redux';
 
 jest.mock('@pagopa/selfcare-common-frontend/decorators/withLogin');
+
+beforeEach(() => {
+  jest.spyOn(global, 'fetch').mockImplementation(
+    jest.fn(() =>
+      Promise.resolve({
+        status: 200,
+        ok: true,
+        json: () => Promise.resolve('https://backofficeURL.it/ui#id=jwtToken'),
+      })
+    ) as jest.Mock
+  );
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
 
 const renderApp = (injectedStore?: ReturnType<typeof createStore>) => {
   const store = injectedStore ? injectedStore : createStore();
@@ -17,9 +33,62 @@ const renderApp = (injectedStore?: ReturnType<typeof createStore>) => {
   return { store };
 };
 
-test('renders the landing page', () => {
-  const { store } = renderApp();
-  verifyLoginMockExecution(store.getState());
+describe('Token Exchange', function () {
+  test('renders the landing page', async () => {
+    let store;
+    const url = 'http://dummy.com/token-exchange?institutionId=123&productId=prod-xxx';
 
-  expect(document.getElementById('tokenExchange'));
+    Object.defineProperty(window, 'location', {
+      value: new URL(url),
+    });
+
+    await waitFor(() => ({ store } = renderApp()));
+    verifyLoginMockExecution(store.getState());
+
+    expect(document.getElementById('tokenExchange'));
+  });
+
+  test('Input institutionId, productId, environment', async () => {
+    const url =
+      'http://dummy.com/token-exchange?institutionId=123&productId=prod-xxx&environment=Collaudo';
+
+    await waitFor(() => (window.location.href = url));
+    await waitFor(() => renderApp());
+
+    expect(window.location.href).toContain('/ui#id=jwtToken');
+  });
+
+  test('Input institutionId, productId, environment and code', async () => {
+    const url =
+      'http://dummy.com/token-exchange?institutionId=123&productId=prod-xxx&environment=Collaudo&code=cod42';
+
+    await waitFor(() => (window.location.href = url));
+    await waitFor(() => renderApp());
+
+    expect(window.location.href).toContain('/ui?code=cod42#id=jwtToken');
+  });
+
+  test('Throw error and click on back button', async () => {
+    Object.defineProperty(window.location, 'assign', {
+      configurable: true,
+      value: jest.fn(),
+    });
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        status: 500,
+        ok: false,
+      })
+    ) as jest.Mock;
+
+    const url =
+      'http://dummy.com/token-exchange?institutionId=123&productId=prod-xxx&environment=Collaudo&code=cod42';
+    await waitFor(() => (window.location.href = url));
+    await waitFor(() => renderApp());
+
+    const goHomeBtn = await screen.findByTestId('go-home-btn-test');
+    expect(goHomeBtn).toBeInTheDocument();
+
+    fireEvent.click(goHomeBtn);
+    expect(window.location.assign).toBeCalledWith('http://selfcare/landing');
+  });
 });
