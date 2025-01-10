4c4405765ddbdedde82d3f300fd27137232fd24d
OraldoDoci <117348307+OraldoDoci@users.noreply.github.com>
2025-01-10 17:10:51 +0100

    removed any from store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
 import { appStateReducer } from '@pagopa/selfcare-common-frontend/lib/redux/slices/appStateSlice';
 import { userReducer } from '@pagopa/selfcare-common-frontend/lib/redux/slices/userSlice';
+import { configureStore, Middleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import { LOG_REDUX_ACTIONS } from '../utils/constants';
 
-const additionalMiddlewares = [LOG_REDUX_ACTIONS ? logger : undefined];
+const additionalMiddlewares: Array<Middleware> = LOG_REDUX_ACTIONS ? [logger as Middleware] : [];
+
 
 export const createStore = () =>
   configureStore({
@@ -12,10 +13,11 @@ export const createStore = () =>
       user: userReducer,
       appState: appStateReducer,
     },
-    middleware: (getDefaultMiddleware: any) =>
+    middleware: (getDefaultMiddleware) =>
       additionalMiddlewares.reduce(
-        (array, middleware) => (middleware ? array.concat(middleware) : array),
-        getDefaultMiddleware({ serializableCheck: false })
+        (array, middleware) =>
+          middleware ? array.concat(middleware) : array,
+        getDefaultMiddleware({ serializableCheck: false }) as Array<Middleware>
       ),
   });
 
